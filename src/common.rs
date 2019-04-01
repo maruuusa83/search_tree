fc1167f94375357ffa4db2737bf777c489c0a101
Daichi Teruya <tmarusa83@gmail.com>
2019-04-01 12:47:02 +0000

    add SearchTreeTrait, which uses associated type

--- src/common.rs
+++ src/common.rs
@@ -13,3 +13,29 @@ pub trait SearchTree<K: PartialOrd, V> {
     fn insert(&mut self, pair: Pair<K, V>);
     fn search(&self, key: K) -> &Option<Box<Node<K, V>>>;
 }
+
+
+//////////////////////////////////////////////////////
+
+pub trait SearchTreeTrait<K: PartialOrd, V> {
+    type N;
+
+    fn insert(&mut self, pair: Pair<K, V>);
+    fn search(&self, key: K) -> &Option<Box<Self::N>>;
+}
+
+pub struct SampleTree<K: PartialOrd, V> {
+    pub root: Option<Box<Node<K, V>>>,
+}
+
+impl<K: PartialOrd, V> SearchTreeTrait<K, V> for SampleTree<K, V> {
+    type N = Node<K, V>;
+
+    fn insert(&mut self, pair: Pair<K, V>) {
+
+    }
+
+    fn search(&self, key: K) -> &Option<Box<Self::N>> {
+        return &Option::None;
+    }
+}
