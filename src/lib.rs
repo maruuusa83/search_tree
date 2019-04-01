ba8583436734525f62f548b7e992d2b767a7c071
Daichi Teruya <tmarusa83@gmail.com>
2019-04-01 04:29:55 +0000

    modify test method

--- src/lib.rs
+++ src/lib.rs
@@ -7,11 +7,11 @@ use common::{Pair, SearchTree};
 use naive_tree::NaiveTree;
 use rand::Rng;
 
-fn random_insert<T: SearchTree<i32, i32>>(mut st: T) {
+fn insert<T: SearchTree<i32, i32>>(mut st: T, index: &[i32]) {
     let mut rng = rand::thread_rng();
 
-    for _i in 0..10000 {
-        let mut pair: Pair<i32, i32> = Pair { key: rng.gen(), value: rng.gen() };
+    for i in 0..index.len() {
+        let mut pair: Pair<i32, i32> = Pair { key: index[i], value: rng.gen() };
         let mut pair_2: Pair<i32, i32> = Pair { key: pair.key, value: pair.value };
         st.insert(pair);
 
@@ -23,9 +23,40 @@ fn random_insert<T: SearchTree<i32, i32>>(mut st: T) {
     }
 }
 
+
+fn random_insert<T: SearchTree<i32, i32>>(mut st: T) {
+    let mut index = [0i32; 10000];
+    for i in 0..index.len() {
+        index[i] = i as i32;
+    }
+
+    let mut rng = rand::thread_rng();
+
+    for i in 0..index.len() {
+        let rand: usize = rng.gen();
+        let j = rand % index.len();
+        index.swap(i, j);
+    }
+
+    insert(st, &index);
+}
+
+fn sequential_insert<T: SearchTree<i32, i32>>(mut st: T) {
+    let mut index = [0i32; 10000];
+    for i in 0..index.len() {
+        index[i] = i as i32;
+    }
+
+    insert(st, &index);
+}
+
+
 #[test]
 fn naive_tree() {
     let nt: NaiveTree<i32, i32> = NaiveTree{ root: None };
     random_insert(nt);
+
+    let nt2: NaiveTree<i32, i32> = NaiveTree{ root: None };
+    sequential_insert(nt2);
 }
 
